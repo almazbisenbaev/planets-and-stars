d0ded6bcf69f6197c763134147e68f02fef469a6
Almaz Bisenbaev <almaz9527@gmail.com>
2025-06-08 22:39:02 +0500

    Added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import { Analytics } from "@vercel/analytics/next"
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,6 +31,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+
+        <Analytics/>
       </body>
     </html>
   );
